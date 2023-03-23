6a519315da352ba728d363c3aa71a1af8a885b2e
Donya82 <123035772+Donya82@users.noreply.github.com>
2023-03-22 18:23:32 -0700

    Update testsuite.py

--- testsuite.py
+++ testsuite.py
@@ -1,12 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-def blinkLED(pin, delay):
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(delay)
-
 def main():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.OUT)
@@ -14,19 +8,28 @@ def main():
     while True:
         # Test 1: blink LED 5 times w on/off intervals 500ms
         for i in range(5):
-            blinkLED(11, 0.5)
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(0.5)
 
-        # Test 2: read output of light sensor w intervals 100 ms
-        # print raw value & "light" or "dark" w threshold 10
+        # Test 2:For about 5 seconds, read the output of the Grove light sensor with intervals of
+#100 ms and print the raw value along with the text “bright” or “dark” (you will have
+#to determine a threshold for this through experimentation).
 
 
-        # Test 3: blink LED 4 times w on/off intervals 200 ms
+        # Test 3: Blink the LED 4 times with on/off intervals of 200ms.
         for i in range(4):
-            blinkLED(11, 0.2)
+            GPIO.output(11, GPIO.HIGH)
+            time.sleep(0.2)
+            GPIO.output(11, GPIO.LOW)
+            time.sleep(0.2)
 
-        # Test 4: read output of sound sensor w intervals 100 ms
-        # print raw value 
-        # if tapped, LED turns on for 100 ms
+
+        # Test 4: For about 5 seconds, read the output of the Grove sound sensor with intervals of
+#100 ms and print the raw value. If the sound sensor is tapped (i.e. the sound
+#magnitude goes above the threshold you decide from experimentation), the LED
+#should turn on for 100 ms.
             
 
 if __name__ == "__main__":
